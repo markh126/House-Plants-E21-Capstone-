f9c4a9147631fba874d81d78f530c9d30a266902
Mark Hamilton <ham.mark126@gmail.com>
2023-02-25 13:08:15 -0600

    added a dropdown to the navbar and changed the name

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
+import { NavDropdown } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 
 export default function NavBar() {
@@ -9,7 +10,7 @@ export default function NavBar() {
       <div className="container-fluid">
         <Link passHref href="/">
           <a className="navbar-brand" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
-            CHANGE ME
+            House Plants
           </a>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,6 +26,23 @@ export default function NavBar() {
                 </a>
               </Link>
             </li>
+
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Houses"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item href="#action/3.1">House 1</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                House 2
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">House 3</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Add a New House
+              </NavDropdown.Item>
+            </NavDropdown>
+
             <button type="button" className="btn btn-danger" onClick={signOut}>
               Sign Out
             </button>
